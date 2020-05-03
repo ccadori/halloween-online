bad37304a2dc427694428e5f9199a1642e9cbcd8
Rafael Barbosa <rafaspb11@gmail.com>
2020-05-03 17:35:22 -0300

    Sound Effects

---
+++ Unity/Assets/Scripts/Sounds/WerewolfHowlingSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WerewolfHowlingSound : MonoBehaviour
+{
+
+    [SerializeField] AudioSource audioSource;
+
+    private void OnEnable()
+    {
+        NetworkManager.OnNight += onNight;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManager.OnNight -= onNight;
+    }
+
+    void onNight()
+    {
+        audioSource.Play();
+    }
+}
