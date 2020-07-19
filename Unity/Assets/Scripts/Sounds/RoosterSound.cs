aea4eb8a5f94a35853e592e656187518448172a3
Rafael Barbosa <rafaspb11@gmail.com>
2020-07-18 23:38:56 -0300

    Rooster Sound On Day

---
+++ Unity/Assets/Scripts/Sounds/RoosterSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RoosterSound : MonoBehaviour
+{
+
+    [SerializeField] AudioSource audioSource;
+
+    private void OnEnable()
+    {
+        NetworkManager.OnNightEnded += OnNightEnded;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManager.OnNightEnded -= OnNightEnded;
+    }
+
+    void OnNightEnded()
+    {
+        audioSource.Play();
+    }
+}
