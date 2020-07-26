9d43e556bae2acbc858130986cdb523c0c6fa5ab
ccadori <carloscadori.dev@gmail.com>
2020-07-26 20:35:18 -0300

    allowing cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,15 @@ const http = require('http');
 const Matchmaking = require('./lib/matchmaking');
 
 const server = http.createServer();
-const io = socketio(server);
+const io = socketio(server, { handlePreflightRequest: (req, res) => {
+  const headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": req.headers.origin,
+      "Access-Control-Allow-Credentials": true
+  };
+  res.writeHead(200, headers);
+  res.end();
+}});
 const matchmaking = new Matchmaking();
 
 io.on("connection", matchmaking.onClientEnter);
